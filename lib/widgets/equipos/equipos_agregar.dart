c075d805fd1c4e06b91f64f9c8b3999d2e03a535
eduardo-solis <solishernandezeduardosolis@gmail.com>
2022-11-28 19:42:26 -0600

    Creando el formulario para agregar el equipo

--- lib/widgets/equipos/equipos_agregar.dart
+++ lib/widgets/equipos/equipos_agregar.dart
@@ -5,10 +5,55 @@ class EquiposAgregar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Agregar equipo"),
       ),
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: "Nombre"),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: "Categoria"),
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: "Año de fundación"),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: "Zona"),
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: "Color Local"),
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: "Color Visitante"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text("Guardar"))
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
