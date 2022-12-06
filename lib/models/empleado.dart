7d28da8f00d6989ff004f6ce179e357e38320e72
eduardo-solis <solishernandezeduardosolis@gmail.com>
2022-12-06 08:19:40 -0600

    Agregando el modelo del empleado

---
+++ lib/models/empleado.dart
@@ -0,0 +1,46 @@
+class Empleado {
+  late int idPersona;
+  late String nombre;
+  late String primerApellido;
+  late String segundoApellido;
+  late String fechaNacimiento;
+  late String sexo;
+  late String telefono;
+  late String telefono2;
+  late String correo;
+  late int idEmpleado;
+  late String calleE;
+  late String numeroE;
+  late String coloniaE;
+  late String codigoPostalE;
+  late int idMunicipioE;
+  late String curpe;
+  late int tipoEmpleado;
+  late String rfcE;
+  late String nssE;
+  late double salarioE;
+  late String horarioE;
+
+  Empleado(
+      this.idPersona,
+      this.idEmpleado,
+      this.nombre,
+      this.primerApellido,
+      this.segundoApellido,
+      this.fechaNacimiento,
+      this.sexo,
+      this.telefono,
+      this.telefono2,
+      this.correo,
+      this.calleE,
+      this.numeroE,
+      this.coloniaE,
+      this.codigoPostalE,
+      this.idMunicipioE,
+      this.curpe,
+      this.tipoEmpleado,
+      this.rfcE,
+      this.nssE,
+      this.salarioE,
+      this.horarioE);
+}
