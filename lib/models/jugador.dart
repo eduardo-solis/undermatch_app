e2860100d861723dddca2e81a6b41049eee411d8
eduardo-solis <solishernandezeduardosolis@gmail.com>
2022-12-06 08:23:57 -0600

    Agregando el modelo del jugador

---
+++ lib/models/jugador.dart
@@ -0,0 +1,32 @@
+class Jugador {
+  late int idPersona;
+  late int idJugador;
+  late String nombre;
+  late String primerApellido;
+  late String segundoApellido;
+  late String fechaNacimiento;
+  late String sexo;
+  late String telefono;
+  late String telefono2;
+  late String correo;
+  late String numDorsal;
+  late String sobreNombre;
+  late String posicion;
+  late int capitan;
+
+  Jugador(
+      this.idPersona,
+      this.idJugador,
+      this.nombre,
+      this.primerApellido,
+      this.segundoApellido,
+      this.fechaNacimiento,
+      this.sexo,
+      this.telefono,
+      this.telefono2,
+      this.correo,
+      this.numDorsal,
+      this.sobreNombre,
+      this.posicion,
+      this.capitan);
+}
